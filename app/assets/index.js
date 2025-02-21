eb745058c3741c932c1c16e7af52c7f6169feb2e
Yash Vaidya <bobbynaldocr7@gmail.com>
2025-02-21 18:07:52 +0530

    hero section

---
+++ app/assets/index.js
@@ -0,0 +1,29 @@
+import hero from './hero.png'
+import child from './child.png'
+import downArrow from './downArrow.png'
+import family from './family.png'
+import girls from './girls.png'
+import heroMobile from './heroMobile.png'
+import laptop from './laptop.png'
+import logo from './logo1.png'
+import mother from './mother1.png'
+import sky from './sky.png'
+import twistedArrow from './twistedArrow.png'
+import star from './star.svg'
+import favicon from './favicon.png'
+
+export {
+    hero,
+    child,
+    downArrow,
+    family,
+    girls,
+    heroMobile,
+    laptop,
+    logo,
+    mother,
+    sky,
+    twistedArrow,
+    star,
+    favicon
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,44 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { hero, heroMobile, star } from "./assets";
 
 export default function Home() {
   return (
-   <div>Justliv</div>
+    <main className="min-h-screen w-full relative">
+      {/* Hero Section */}
+      <section className="relative h-screen max-h-full w-full overflow-hidden rounded-b-[40px]">
+        <Image
+          src={heroMobile}
+          alt="Children playing with colorful parachute"
+          fill
+          className="md:hidden"
+          priority
+        />
+        <Image
+          src={hero}
+          alt="Children playing with colorful parachute"
+          fill
+          className="hidden md:block"
+          priority
+        />
+        <div className="absolute inset-0 flex flex-col items-start lg:items-center justify-end text-white pb-20 md:items-start leading-tight px-5 py-10">
+          <h1 className="text-left lg:text-center text-[27px] md:text-[38px] font-normal mb-4 font-serif leading-tight">
+            Your Child's Perfect Schedule &nbsp;
+            <br className="hidden sm:block" />
+            in Just a Few Clicks
+          </h1>
+          <p className="mb-8 text-[18px] sm:text-xl font-normal">Less Planning, More Living.</p>
+
+          <Button className="inline-flex cursor-pointer items-center px-8 py-4 rounded-lg bg-[#FF97A8] hover:bg-[#FF97A8] transition-colors duration-200 text-white font-semibold text-lg">
+            <Image
+              src={star}
+              alt="star Logo"
+              className="mx-auto"
+            />
+            <span className="text-black font-medium text-[18px] font-lato tracking-[-0.5px]">Join the waiting list</span>
+          </Button>
+        </div>
+      </section>
+    </main>
   );
 }
